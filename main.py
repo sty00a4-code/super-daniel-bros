07d071c70a75529b7348d170b0ca3b3ca8269490
sty00a4-code <sty00a4.code@gmail.com>
2025-03-17 12:13:58 +0100

    Co-authored-by: DonaldDGD <DonaldDGD@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import pygame as pg
+from pygame import *
+
+pg.init()
+screen = pg.display.set_mode((1920 / 2, 1080 / 2), )
+clock = pg.time.Clock()
+running = True
+
+while running:
+    for e in event.get():
+        if e.type == QUIT:
+            running = False
+
+    screen.fill("cyan")
+    # Draw Start
+    draw.rect(screen, Color(255, 0, 0), Rect(0, 0, 50, 50))
+    # Draw End
+    display.flip()
+    clock.tick(60)
+
+pg.quit()
