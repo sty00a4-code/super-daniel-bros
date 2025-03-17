1388d522087127040f584e16900984f9304ce47b
sty00a4-code <sty00a4.code@gmail.com>
2025-03-17 16:31:32 +0100

    load test tilemap

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ window = display.set_mode((1920 / 2, 1080 / 2))
 screen = Surface((1920 / 4, 1080 / 4))
 display.set_caption("Super Daniel Bros")
 clock = time.Clock()
+camera = Vector2(0, 0)
+tilemap = load_map("test")
 
 while True:
     for e in event.get():
@@ -16,7 +18,7 @@ while True:
     window.fill("black")
     screen.fill("cyan")
     # Draw Start
-    draw.rect(screen, Color(255, 0, 0), Rect(0, 0, 50, 50))
+    tilemap.draw(screen, camera)
     # Draw End
     window.blit(transform.scale(screen, window.get_size()), (0, 0))
     display.flip()
